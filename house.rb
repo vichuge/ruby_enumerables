70db049f81aa2827f59797c8d52fad040a6680b4
vichuge <victor.hugo.pacheco.flores@gmail.com>
2020-12-03 16:35:24 -0600

    Fix details in project

--- house.rb
+++ house.rb
@@ -1,5 +1,5 @@
-rubocop:disable Metrics/CyclomaticComplexity
-rubocop:disable Metrics/PerceivedComplexity
+#rubocop:disable Metrics/CyclomaticComplexity
+#rubocop:disable Metrics/PerceivedComplexity
 
 module Enumerable
   def my_each
@@ -132,5 +132,5 @@ module Enumerable
   end
 end
 
-rubocop:enable Metrics/CyclomaticComplexity
-rubocop:enable Metrics/PerceivedComplexity
+#rubocop:enable Metrics/CyclomaticComplexity
+#rubocop:enable Metrics/PerceivedComplexity
