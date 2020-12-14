557fd4c850815ed0e49165809b3b037d87b401ec
unknown <victor.hugo.pacheco.flores@gmail.com>
2020-12-14 16:59:58 -0600

    Remove a blank space inside an it

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -7,13 +7,13 @@ describe Enumerable do
     end
 
     it "return the same array, but print only the values according the if statement" do
-    expect([1, 2, 3, 4].my_each { |element| print element if element.even?}).to eql([1, 2, 3, 4])
+      expect([1, 2, 3, 4].my_each { |element| print element if element.even?}).to eql([1, 2, 3, 4])
     end
   end
 
   describe ".my_each_with_index" do
     it "return the same array, but print values according the requirements using an index for the elements" do
-        expect(%w[apple banana strawberry pineapple].each_with_index { |fruit, index| print fruit if index.even? }).to eql(%w[apple banana strawberry pineapple])
+      expect(%w[apple banana strawberry pineapple].each_with_index { |fruit, index| print fruit if index.even? }).to eql(%w[apple banana strawberry pineapple])
     end
   end
 end
