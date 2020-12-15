81c3c71cb26740375a80780cafe9b0d2b9b7e746
Miguel Ricaño <mricanho@gmail.com>
2020-12-15 09:41:12 -0600

    Add my_select and my_all

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,6 +1,7 @@
 require './main'
 
 describe Enumerable do
+  let(:array) { [1, 2, 3, 4, 5, 6] }
   describe ".my_each" do
     it "return the same array, but print the result according the block" do
       expect([1, 2, 3, 4].my_each { |element| print element + 1}).to eql([1, 2, 3, 4])
@@ -16,4 +17,22 @@ describe Enumerable do
       expect(%w[apple banana strawberry pineapple].each_with_index { |fruit, index| print fruit if index.even? }).to eql(%w[apple banana strawberry pineapple])
     end
   end
+
+  describe '.my_select' do
+    it 'returns an array with the items that match yield pattern' do
+      expect(array.my_select(&:even?)).to eql([2, 4, 6])
+    end
+  end
+
+  describe '.my_all?' do
+    it 'unless block_given, returns true if all items are true' do
+      expect(array.my_all?).to eql(true)
+    end
+
+    it "doesn't return true if an item is false or nil" do
+      expect([nil, true, 99].my_all?).to eql(false) 
+    end
+  end
 end
+
+  
